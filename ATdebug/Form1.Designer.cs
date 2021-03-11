531d917985ad3effbc664201ab6d568d88744616
Roar Almaas <roar.almas@gmail.com>
2021-03-11 17:10:04 +0100

    Version 1.2

--- ATdebug/Form1.Designer.cs
+++ ATdebug/Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace ATdebug
             this.label2 = new System.Windows.Forms.Label();
             this.tbDataIn = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this._serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.pbConnectStatus = new System.Windows.Forms.PictureBox();
             this.btnCWJAP = new System.Windows.Forms.Button();
             this.tbSSID = new System.Windows.Forms.TextBox();
             this.tbPasswd = new System.Windows.Forms.TextBox();
@@ -59,14 +59,18 @@ namespace ATdebug
             this.tbWeb = new System.Windows.Forms.TextBox();
             this.btnTest = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbBitcoin = new System.Windows.Forms.TextBox();
+            this.labelBitcoin = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbConnectStatus)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbSelectCOM
             // 
             this.cbSelectCOM.FormattingEnabled = true;
-            this.cbSelectCOM.Location = new System.Drawing.Point(44, 19);
+            this.cbSelectCOM.Location = new System.Drawing.Point(44, 53);
             this.cbSelectCOM.Name = "cbSelectCOM";
             this.cbSelectCOM.Size = new System.Drawing.Size(121, 21);
             this.cbSelectCOM.TabIndex = 0;
@@ -74,7 +78,7 @@ namespace ATdebug
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(90, 79);
+            this.btnRefresh.Location = new System.Drawing.Point(90, 17);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(75, 23);
             this.btnRefresh.TabIndex = 1;
@@ -95,7 +99,7 @@ namespace ATdebug
             // cbSelectBaud
             // 
             this.cbSelectBaud.FormattingEnabled = true;
-            this.cbSelectBaud.Location = new System.Drawing.Point(44, 52);
+            this.cbSelectBaud.Location = new System.Drawing.Point(44, 86);
             this.cbSelectBaud.Name = "cbSelectBaud";
             this.cbSelectBaud.Size = new System.Drawing.Size(121, 21);
             this.cbSelectBaud.TabIndex = 3;
@@ -104,7 +108,7 @@ namespace ATdebug
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Location = new System.Drawing.Point(9, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 4;
@@ -113,7 +117,7 @@ namespace ATdebug
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 52);
+            this.label2.Location = new System.Drawing.Point(3, 86);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
@@ -130,23 +134,26 @@ namespace ATdebug
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pbConnectStatus);
             this.groupBox1.Controls.Add(this.cbSelectCOM);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.cbSelectBaud);
             this.groupBox1.Controls.Add(this.btnRefresh);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbSelectBaud);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(16, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(186, 113);
+            this.groupBox1.Size = new System.Drawing.Size(174, 113);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Port:";
             // 
-            // _serialPort
+            // pbConnectStatus
             // 
-            this._serialPort.Handshake = System.IO.Ports.Handshake.RequestToSend;
-            this._serialPort.PortName = "COM15";
-            this._serialPort.RtsEnable = true;
+            this.pbConnectStatus.Location = new System.Drawing.Point(44, 17);
+            this.pbConnectStatus.Name = "pbConnectStatus";
+            this.pbConnectStatus.Size = new System.Drawing.Size(35, 23);
+            this.pbConnectStatus.TabIndex = 6;
+            this.pbConnectStatus.TabStop = false;
             // 
             // btnCWJAP
             // 
@@ -264,7 +271,7 @@ namespace ATdebug
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(208, 15);
+            this.label7.Location = new System.Drawing.Point(199, 15);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(71, 13);
             this.label7.TabIndex = 22;
@@ -272,7 +279,7 @@ namespace ATdebug
             // 
             // tbHost
             // 
-            this.tbHost.Location = new System.Drawing.Point(208, 32);
+            this.tbHost.Location = new System.Drawing.Point(199, 32);
             this.tbHost.Name = "tbHost";
             this.tbHost.Size = new System.Drawing.Size(93, 20);
             this.tbHost.TabIndex = 23;
@@ -280,7 +287,7 @@ namespace ATdebug
             // 
             // tbPing
             // 
-            this.tbPing.Location = new System.Drawing.Point(208, 79);
+            this.tbPing.Location = new System.Drawing.Point(199, 79);
             this.tbPing.Name = "tbPing";
             this.tbPing.Size = new System.Drawing.Size(93, 20);
             this.tbPing.TabIndex = 25;
@@ -289,7 +296,7 @@ namespace ATdebug
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(208, 62);
+            this.label8.Location = new System.Drawing.Point(199, 62);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(56, 13);
             this.label8.TabIndex = 24;
@@ -314,7 +321,7 @@ namespace ATdebug
             // 
             // btnTest
             // 
-            this.btnTest.Location = new System.Drawing.Point(216, 102);
+            this.btnTest.Location = new System.Drawing.Point(207, 105);
             this.btnTest.Name = "btnTest";
             this.btnTest.Size = new System.Drawing.Size(75, 23);
             this.btnTest.TabIndex = 6;
@@ -329,13 +336,41 @@ namespace ATdebug
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(168, 13);
             this.label10.TabIndex = 29;
-            this.label10.Text = "ATcommand ver. 1.1 @ralm/2021";
+            this.label10.Text = "ATcommand ver. 1.2 @ralm/2021";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbBitcoin
+            // 
+            this.tbBitcoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbBitcoin.Location = new System.Drawing.Point(346, 107);
+            this.tbBitcoin.Name = "tbBitcoin";
+            this.tbBitcoin.Size = new System.Drawing.Size(220, 35);
+            this.tbBitcoin.TabIndex = 30;
+            this.tbBitcoin.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbBitcoin.Visible = false;
+            // 
+            // labelBitcoin
+            // 
+            this.labelBitcoin.AutoSize = true;
+            this.labelBitcoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBitcoin.Location = new System.Drawing.Point(321, 79);
+            this.labelBitcoin.Name = "labelBitcoin";
+            this.labelBitcoin.Size = new System.Drawing.Size(245, 29);
+            this.labelBitcoin.TabIndex = 31;
+            this.labelBitcoin.Text = "Current Bitcoin rate:";
+            this.labelBitcoin.Visible = false;
             // 
             // ATcontrol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(620, 285);
+            this.Controls.Add(this.labelBitcoin);
+            this.Controls.Add(this.tbBitcoin);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.label9);
@@ -358,6 +393,7 @@ namespace ATdebug
             this.Text = "ATcommand";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbConnectStatus)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -375,7 +411,7 @@ namespace ATdebug
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbDataIn;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.IO.Ports.SerialPort _serialPort;
+        // private System.IO.Ports.SerialPort _serialPort;
         private System.Windows.Forms.Button btnCWJAP;
         private System.Windows.Forms.TextBox tbSSID;
         private System.Windows.Forms.TextBox tbPasswd;
@@ -396,6 +432,10 @@ namespace ATdebug
         private System.Windows.Forms.TextBox tbWeb;
         private System.Windows.Forms.Button btnTest;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pbConnectStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tbBitcoin;
+        private System.Windows.Forms.Label labelBitcoin;
     }
 }
 
--- ATdebug/Form1.cs
+++ ATdebug/Form1.cs
@@ -1,21 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 using System.Runtime.InteropServices;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ATdebug
 {
     public partial class ATcontrol : Form
     {
         string[] baudrates = { "9600", "19200", "38400", "57600", "74880", "115200" };
-        string[] ATcommands = { "Restore ESP-01 _AT+RESTORE", "Reset ESP-01 _AT+RST", "Show info _AT+GMR",
+        string[] ATcommands = { "Select a command _x", "Restore ESP-01 _AT+RESTORE", "Reset ESP-01 _AT+RST", "Show info _AT+GMR",
             "Show SYSRAM _AT+SYSRAM?", "Show UART setup _AT+UART_CUR?",
             "Show connection status _AT+CIPSTATUS", "Show AP Connection _AT+CWJAP?",
             "WiFi mode Client _AT+CWMODE=1", "WiFi mode Server _AT+CWMODE=2",
@@ -25,14 +23,16 @@ namespace ATdebug
             "Show AP-survey _AT+CWLAP", 
             "Local Echo Off _ATE0", "Local Echo On _ATE1",  };
         // private SerialPort port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
-        // static SerialPort _serialPort;
-        string dataIn;
+
+        static SerialPort _serialPort;
+        private string dataIn;
         string wifi_SSID;
         string wifi_PWD;
         string host_name;
         string host_IP;
-        int uartFlag = 0;
-
+        int flagBitcoin = 0;
+        int flagBitCoinDisplay = 0;
+ 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
         const int EM_SETCUEBANNER = 0x1501;
@@ -42,12 +42,14 @@ namespace ATdebug
             InitializeComponent();
 
             _serialPort = new SerialPort();
+            //==    this.myDelegate = new AddDataDelegate(retrieveSerial);
             UpdateCOM();
             InitBaud();
             InitATcommand();
             // Put something in the TB to indicate expected format
             // tbWeb.Text = "http://192.168.2.111/index.html";
             SendMessage(tbWeb.Handle, EM_SETCUEBANNER, 1, "http://192.168.2.111/index.html");
+            this.timer1.Enabled = true;
         }
 
         private void UpdateCOM()
@@ -108,6 +110,7 @@ namespace ATdebug
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            this.timer1.Enabled = false;
             this.Close();
         }
 
@@ -120,7 +123,7 @@ namespace ATdebug
                 _serialPort.BaudRate = Convert.ToInt32(cbSelectBaud.Text);
                 _serialPort.RtsEnable = false;
                 _serialPort.DtrEnable = false;
-                _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataReceived);
+                _serialPort.Handshake = Handshake.None;
 
                 _serialPort.Open();
             }
@@ -130,18 +133,122 @@ namespace ATdebug
             }
         }
 
- 
+        private void parseJson(string data)
+        {
+            var bitcoin = JObject.Parse(data);
+            string dollar = (string)bitcoin["bpi"]["USD"]["rate_float"];
+            tbBitcoin.Text = dollar;
+            tbDataIn.Visible = false;
+            tbBitcoin.Visible = true;
+            labelBitcoin.Visible = true;
+            flagBitCoinDisplay = 5;
+            // tbDataIn.AppendText(bitcoin["bpi"]["USD"]["rate_float"]);
+            int i = 12;
+
+        }
+
+        // sendCmd accept two arguments:
+        //  cmd which is the command to be sent
+        //  timeout is number of seconds to wait for an OK or ERROR
+        private Boolean sendCmd(string cmd, int timeout)
+        {
+            int uartFlag = 0;
+            int counter = 0;
+
+            this.timer1.Stop();             // Disable reading UART data at two places
+            saveLog(cmd, 1);
+            _serialPort.DiscardInBuffer();
+            if (cmd.Length > 0)
+                _serialPort.Write(cmd);
+            _serialPort.Write("\r\n");
+            tbDataIn.AppendText("==> Entering sendCmd <===\r\n");
+            dataIn = "";
+            long now = DateTimeOffset.Now.ToUnixTimeSeconds();
+            while (uartFlag == 0)
+            {
+                // just wait for a reply
+                dataIn += _serialPort.ReadExisting();
+                if (dataIn.Contains("OK"))
+                {
+                    uartFlag = 1;
+                }
+                if (dataIn.Contains("ERROR"))
+                {
+                    uartFlag = -1;
+                }
+                if (dataIn.Contains("CONNECT")) { 
+                    pbConnectStatus.BackColor = Color.Green;
+                    tbDataIn.AppendText("RECEIVED CONNECT\r\n");
+                }
+                if (dataIn.Contains("CLOSED"))
+                {
+                    pbConnectStatus.BackColor = Color.Red;
+                    uartFlag = 1;
+                }
+                if (dataIn.Contains("+CIPDOMAIN:"))
+                {
+                    int start = dataIn.IndexOf(':');
+                    int end = dataIn.IndexOf('\r', start);
+                    if (end > start)
+                        tbPing.Text = dataIn.Substring(start + 1, end - start - 1);
+                }
+                if ((DateTimeOffset.Now.ToUnixTimeSeconds() - now) >= timeout)
+                {
+                    uartFlag = -2;
+                    tbDataIn.AppendText("TIMEOUT REACHED\r\n");
+                }
+                Thread.Yield();
+                Task.Delay(200).Wait();
+                counter++;
+            }
+            tbDataIn.AppendText(dataIn);
+
+            /*
+             * Check if this was a bitcoin test
+             */
+            if ((flagBitcoin == 1) && dataIn.Contains("200 OK"))
+            {
+                int header = dataIn.IndexOf("+IPD");
+                if (header > 0)
+                {
+                    // Get Body part
+                    int body = dataIn.IndexOf("\r\n\r\n", header);
+                    if (body > 0) {
+                        body += 4;
+                        int end = dataIn.IndexOf("CLOSED", body);
+                        if (body > 0)
+                        {
+                            // MessageBox.Show(dataIn.Substring(body, end-body), "BitCoin");
+                            saveLog(dataIn.Substring(body, end - body), 3);
+                            parseJson(dataIn.Substring(body, end - body));
+                        }
+                        else
+                        {
+                            // MessageBox.Show(dataIn.Substring(body), "BitCoin");
+                            saveLog(dataIn.Substring(body), 3);
+                            parseJson(dataIn.Substring(body));
+                        }
+                    }
+                }
+
+                flagBitcoin = 0;
+            }
+            tbDataIn.AppendText("==> Leaving sendCmd <===\r\n");
+            this.timer1.Start();             // Enable reading UART data in function "timer1_Tick"
+            if (uartFlag == 1)
+                return true;
+            else
+                return false;   // ERROR or timeout
+        }
 
         private void tbSSID_TextChanged(object sender, EventArgs e)
         {
             wifi_SSID = tbSSID.Text;
-            tbDebug.Text = wifi_SSID;
         }
 
         private void tbPasswd_TextChanged(object sender, EventArgs e)
         {
             wifi_PWD = tbPasswd.Text;
-            // tbDebug.Text = wifi_PWD;
             btnCWJAP.Enabled = true;
         }
 
@@ -149,16 +256,15 @@ namespace ATdebug
         {
             // Connect to WLAN
             string buffer;
+            // First set mode to client
+            sendCmd("AT+CWMODE=1", 10);
+
             buffer = "AT+CWJAP=\"" + wifi_SSID + "\",\"" + wifi_PWD + "\"";
-            tbDebug.Text = buffer;
             if (_serialPort.IsOpen)
             {
-                _serialPort.Write(buffer);
-                _serialPort.Write("\r\n");
+                sendCmd(buffer, 10);
             }
         }
-
- 
 
         private void btnClear_Click(object sender, EventArgs e)
         {
@@ -183,17 +289,19 @@ namespace ATdebug
                         if (j > 0)
                         {
                             j++;
-                            command = cmd.Substring(j);
+                            if (cmd.Substring(j).Length > 2)
+                                command = cmd.Substring(j);
                         }
                         break;
                     }
                 }
                 if (j > 0)
                 {
-                    _serialPort.Write(command);
-                    _serialPort.Write("\r\n");
+                    if (command.Length > 2)
+                        sendCmd(command, 10);
                 }
             }
+            cbATcommand.SelectedIndex = 0;
         }
  
         private void tbHost_KeyDown(object sender, KeyEventArgs e)
@@ -205,8 +313,7 @@ namespace ATdebug
                 if (_serialPort.IsOpen)
                 {
                     string buffer = "AT+CIPDOMAIN=\"" + tbHost.Text + "\"";
-                    _serialPort.Write(buffer);
-                    _serialPort.Write("\r\n");
+                    sendCmd(buffer, 10);
                 }
             }
         }
@@ -221,8 +328,7 @@ namespace ATdebug
                 {
                     string buffer = "AT+PING=\"" + tbPing.Text + "\"";
                     tbDebug.Text = buffer;
-                    _serialPort.Write(buffer);
-                    _serialPort.Write("\r\n");
+                    sendCmd(buffer, 10);
                 }
             }
         }
@@ -234,8 +340,7 @@ namespace ATdebug
                 string buffer = tbManuel.Text;
                 if (_serialPort.IsOpen)
                 {
-                    _serialPort.Write(buffer);
-                    _serialPort.Write("\r\n");
+                    sendCmd(buffer, 10);
                 }
             }
         }
@@ -256,8 +361,7 @@ namespace ATdebug
                         //          User-agent: ESP-01
                         //          Host: <host-part>/r/n\r\n
                         // 4. Send AT+CIPSEND=4,<total length incl \r\n>
-                        _serialPort.Write(buffer);
-                        _serialPort.Write("\r\n");
+                        sendCmd(buffer, 10);
                     }
                 }
             }
@@ -265,48 +369,115 @@ namespace ATdebug
 
         private void btnTest_Click(object sender, EventArgs e)
         {
+            Boolean status = false;
+            Boolean cont = true;
+
             if (_serialPort.IsOpen)
             {
-                _serialPort.Write("AT+CIPMUX=0");
-                _serialPort.Write("\r\n");
-                Task.Delay(100).Wait();
-                _serialPort.Write("AT+CIPSTART=\"TCP\",\"api.coindesk.com\",80");
-                _serialPort.Write("\r\n");
-                Task.Delay(5000).Wait();
-                _serialPort.Write("AT+CIPSEND=86");
-                _serialPort.Write("\r\n");
-                Task.Delay(5000).Wait();
-                _serialPort.Write("GET /v1/bpi/currentprice.json HTTP/1.1");
-                _serialPort.Write("\r\n");
-                Task.Delay(200).Wait();
-                _serialPort.Write("User-agent: ESP-01");
-                _serialPort.Write("\r\n");
-                Task.Delay(200).Wait();
-                _serialPort.Write("Host: api.coindesk.com");
-                _serialPort.Write("\r\n\r\n");
-                Task.Delay(200).Wait();
+                // Prepare to analyze the returned json-data
+                flagBitcoin = 1;
 
+                saveLog("Starting:", 1);
+                saveLog("AT+CIPMUX=0", 1);
+                
+                status = sendCmd("AT+CIPMUX=0", 5);
+                if (!status)
+                {
+                    tbDebug.Text = "Timeout CIPMUX";
+                    cont = false;
+                }
+                if (status && cont)
+                {
+                    saveLog("AT+CIPSTART", 10);
+                    status = sendCmd("AT+CIPSTART=\"TCP\",\"api.coindesk.com\",80", 10);
+                    if (!status)
+                    {
+                        tbDebug.Text = "Timeout CIPSTART";
+                        cont = false;
+                    }
+                } 
+
+                if (status && cont)
+                {
+                    status = sendCmd("AT+CIPSEND=105", 10);
+                    if (!status)
+                    {
+                        tbDebug.Text = "Timeout CIPSEND";
+                        cont = false;
+                    }
+                }
+
+                if (status && cont)
+                {
+                    _serialPort.Write("GET /v1/bpi/currentprice.json HTTP/1.1");
+                    _serialPort.Write("\r\n");
+                    _serialPort.Write("User-agent: ESP-01");
+                    _serialPort.Write("\r\n");
+                    _serialPort.Write("Host: api.coindesk.com");
+                    _serialPort.Write("\r\n");
+                    _serialPort.Write("Connection: close");
+                    _serialPort.Write("\r\n");
+                    status = sendCmd("", 30);
+                    if (!status)
+                    {
+                        tbDebug.Text = "Timeout GET";
+                        cont = false;
+                    }
+                }
             }
         }
 
-        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        private void saveLog(string line, int direction)
         {
-            dataIn = _serialPort.ReadExisting();
-            this.Invoke(new EventHandler(showData));
+            string fn = @"c:\temp\debug.log";
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(fn, true))
+            {
+                switch (direction)
+                {
+                    case 0:
+                        file.Write("<== ");
+                        break;
+                    case 1:
+                        file.Write("==> ");
+                        break;
+                    default:
+                        file.Write("<=> ");
+                        break;
+                }
+
+                file.WriteLine(line);
+            }
         }
 
-        private void showData(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            // Some interpretation of received data....
-            tbDataIn.AppendText(dataIn);
-            if (dataIn.Contains("OK"))
-                if (dataIn.Contains("+CIPDOMAIN"))
+            // Any Bitcoin display
+            if (flagBitCoinDisplay > 0)
+            {
+                flagBitCoinDisplay--;
+                if (flagBitCoinDisplay == 0)
                 {
-                    int i = dataIn.IndexOf(':');
-                    int j = dataIn.IndexOf('\r');
-                    tbPing.Text = dataIn.Substring(i + 1, j - i - 1);
-
+                    tbDataIn.Visible = true;
+                    tbBitcoin.Visible = false;
+                    labelBitcoin.Visible = false;
                 }
+            }
+            if (_serialPort.IsOpen)
+            {
+                string temp = _serialPort.ReadExisting();
+                if (temp.Length > 1)
+                {
+                    saveLog(temp, 3);
+                    tbDataIn.AppendText("Received by timer routine:\r\n");
+                    tbDataIn.AppendText(temp);
+
+                    if (temp.Contains("CONNECT"))
+                        pbConnectStatus.BackColor = Color.Green;
+                    if (temp.Contains("CLOSED"))
+                        pbConnectStatus.BackColor = Color.Red;
+                }
+            }
         }
     }
 }
